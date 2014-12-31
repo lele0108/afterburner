d1be94e09408d1a8cf94ce2d5d05067a626fdb56
Jimmy Liu <sicong.liu98@gmail.com>
2014-12-30 18:50:23 -0800

    picture viewer working

--- Afterburner/choosePhotoController.m
+++ Afterburner/choosePhotoController.m
@@ -11,7 +11,8 @@
 #import "MWCommon.h"
 #import "SVHTTPRequest.h"
 
-#define API_KEY @"eed7d3ac0db9d9d7c6177e51db70f78c"
+#define API_KEY @"c0a46f8bf9dd54d3f2f2662ef84977d8"
+#define SECRET @"c95a269917cd4b9a"
 
 @interface choosePhotoController () <UITableViewDataSource, UITableViewDelegate>
 @end
@@ -21,11 +22,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.photos = [[NSMutableArray alloc] init];
+    self.thumbs = [[NSMutableArray alloc] init];
     [[SVHTTPClient sharedClient] setBasePath:@"https://api.flickr.com/services/rest/"];
     
     NSDictionary *queryParams = @{@"method" : @"flickr.photos.getRecent",
                                   @"api_key" : API_KEY,
-                                  @"extras" : @"url_l",
+                                  @"extras" : @"url_l,url_sq",
                                   @"format" : @"json",
                                   @"nojsoncallback" : @"1"};
     
@@ -35,8 +38,26 @@
                               if (!error) {
                                   NSArray *temp = response[@"photos"][@"photo"];
                                   for (id object in temp) {
-                                      NSLog(@"Value of array = %@", object);
+                                      NSDictionary *photodata = object;
+                                      [self.photos addObject:[MWPhoto photoWithURL:[NSURL URLWithString:photodata[@"url_l"]]]];
+                                      [self.thumbs addObject:[MWPhoto photoWithURL:[NSURL URLWithString:photodata[@"url_sq"]]]];
                                   }
+                                  MWPhotoBrowser *browser = [[MWPhotoBrowser alloc] initWithDelegate:self];
+                                  
+                                  // Set options
+                                  browser.displayActionButton = NO; // Show action button to allow sharing, copying, etc (defaults to YES)
+                                  browser.displayNavArrows = NO; // Whether to display left and right nav arrows on toolbar (defaults to NO)
+                                  browser.displaySelectionButtons = YES; // Whether selection buttons are shown on each image (defaults to NO)
+                                  browser.zoomPhotosToFill = YES; // Images that almost fill the screen will be initially zoomed to fill (defaults to YES)
+                                  browser.alwaysShowControls = NO; // Allows to control whether the bars and controls are always visible or whether they fade away to show the photo full (defaults to NO)
+                                  browser.enableGrid = YES; // Whether to allow the viewing of all the photo thumbnails on a grid (defaults to YES)
+                                  browser.startOnGrid = YES; // Whether to start on the grid of thumbnails instead of the first photo (defaults to NO)
+                                  //browser.wantsFullScreenLayout = YES; // iOS 5 & 6 only: Decide if you want the photo browser full screen, i.e. whether the status bar is affected (defaults to YES)
+                                  
+                                  // Optionally set the current visible photo before displaying
+                                  
+                                  // Present
+                                  [self.navigationController pushViewController:browser animated:YES];
                                   //do stuff
                               } else {
                                   NSLog(@"%@", @"There was a error with the API call");
@@ -45,8 +66,20 @@
 
 }
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+- (NSUInteger)numberOfPhotosInPhotoBrowser:(MWPhotoBrowser *)photoBrowser {
+    return self.photos.count;
+}
+
+- (id <MWPhoto>)photoBrowser:(MWPhotoBrowser *)photoBrowser thumbPhotoAtIndex:(NSUInteger)index {
+    if (index < _thumbs.count)
+        return [_thumbs objectAtIndex:index];
+    return nil;
+}
+
+- (id <MWPhoto>)photoBrowser:(MWPhotoBrowser *)photoBrowser photoAtIndex:(NSUInteger)index {
+    if (index < self.photos.count)
+        return [self.photos objectAtIndex:index];
+    return nil;
 }
 
 
