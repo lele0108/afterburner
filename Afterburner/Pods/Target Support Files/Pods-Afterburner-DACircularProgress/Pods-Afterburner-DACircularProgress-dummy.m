2baf84b955c28af4c76a4da5d336a927a718e5f0
Jimmy Liu <sicong.liu98@gmail.com>
2015-01-30 21:52:40 -0800

    FUCK U XCODE

--- Afterburner/Pods/Target Support Files/Pods-Afterburner-DACircularProgress/Pods-Afterburner-DACircularProgress-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Afterburner_DACircularProgress : NSObject
-@end
-@implementation PodsDummy_Pods_Afterburner_DACircularProgress
-@end
--- Afterburner/Pods/Target Support Files/Pods-Afterburner-PSTCollectionView/Pods-Afterburner-PSTCollectionView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Afterburner_PSTCollectionView : NSObject
-@end
-@implementation PodsDummy_Pods_Afterburner_PSTCollectionView
-@end
